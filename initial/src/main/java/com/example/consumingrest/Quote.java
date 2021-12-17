08bdc7ba415e9c55b80904b35353866b1ad401fd
Tr1ckC0 <trick.co@yahoo.com>
2021-12-17 13:36:30 -0600

    followed instructions and understood

--- initial/src/main/java/com/example/consumingrest/Quote.java
+++ initial/src/main/java/com/example/consumingrest/Quote.java
@@ -2,7 +2,7 @@ package com.example.consumingrest;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Quote {
     private String type;
     private Value value;
